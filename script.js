29b99bc8c8dbe79a6ab00cba3b11affeaa528cd7
GonAvi <dgaduff@gmail.com>
2023-10-25 09:57:21 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const sumar = (num1, num2) => num1 + num2
+const restar = (num1, num2) => num1 - num2
+const multiplicar = (num1, num2) => num1 * num2
+const dividir = (num1, num2) => num1 / num2
+
+const num1 = "Hola "
+const num2 = "a"
+
+console.log(sumar(num1, num2))
+console.log(restar(num1, num2))
+console.log(dividir(num1, num2))
+console.log(multiplicar(num1, num2))
